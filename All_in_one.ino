6ee56db1f03a7b6e42c1f20d09669bf48d25608c
YunChoi <yunhoho@gmail.com>
2017-01-25 17:43:19 -0600

    Update All_in_one.ino

--- All_in_one.ino
+++ All_in_one.ino
@@ -1,4 +1,4 @@
-int emotion = 3; //1 for angry;2 for happy; 3 for sad;
+int emotion = 7; //1 for angry;2 for happy; 3 for sad; 4 for fear; 5 for disgust; 6 for Love; 7 for suprise
 
 #include <FastLED.h>
 
@@ -8,14 +8,11 @@ Adafruit_MPL115A2 mpl115a2;
 float pressureKPA1 = 0, temperatureC1 = 0;
 float pressureKPA2 = 0, temperatureC2 = 0;
 
-#define TEMPERATURE_1 HighPressureSodium
-#define TEMPERATURE_2 Candle
-#define TEMPERATURE_3 ClearBlueSky
-// How many seconds to show each temperature before switching
-#define DISPLAYTIME 20
-// How many seconds to show black between switches
-#define BLACKTIME   3
-#define LED_PIN     4
+//#define TEMPERATURE_1 HighPressureSodium
+//#define TEMPERATURE_2 Candle
+//#define TEMPERATURE_3 ClearBlueSky
+
+#define LED_PIN     4 //Data for LED
 // Information about the LED strip itself
 #define NUM_LEDS    2
 #define CHIPSET     WS2812
@@ -23,7 +20,7 @@ float pressureKPA2 = 0, temperatureC2 = 0;
 CRGB leds[NUM_LEDS];
 #define BRIGHTNESS  64
 
-int buttonstate = 0;
+// int buttonstate = 0;
 void setup()
 {
   delay( 3000 ); // power-up safety delay
@@ -31,26 +28,27 @@ void setup()
   // so that colors can be more accurately rendered through the 'temperature' profiles
   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalSMD5050 );
   FastLED.setBrightness( BRIGHTNESS );
-  mpl115a2.begin();
+  
+  mpl115a2.begin(); // Initiate Pressure sensors
 
-  pinMode(11, INPUT);//PUSH BUTTON
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);//
+ // pinMode(11, INPUT);//PUSH BUTTON
   pinMode(7, OUTPUT);//RESET SENSOR
   pinMode(8, OUTPUT);//RESET SENSOR
-  pinMode(9, OUTPUT);//
-  pinMode(10, OUTPUT);//
+  
+  pinMode(5, OUTPUT);// RIGHT_DOWN
+  pinMode(6, OUTPUT);// RIGHT_UPPER
+  pinMode(9, OUTPUT);//LEFT_DOWN
+  pinMode(10, OUTPUT);//LEFT_UPPER
 
   Serial.begin(9600);
 }
 int i;
 void loop()
 {
-
+  //Pressure sensor 1
   digitalWrite(7, HIGH);
   digitalWrite(8, LOW);
   pressureKPA1 = mpl115a2.getPressure();
-
   Serial.print("Pressure A (kPa): ");
   Serial.print(pressureKPA1, 4);
   Serial.println(" kPa");
@@ -58,7 +56,8 @@ void loop()
   Serial.print("Temp A (*C): ");
   Serial.print(temperatureC1, 1);
   Serial.println(" *C");
-
+  
+  //Pressure sensor 2
   digitalWrite(7, LOW);
   digitalWrite(8, HIGH);
   pressureKPA2 = mpl115a2.getPressure();
@@ -69,117 +68,165 @@ void loop()
   Serial.print("Temp B (*C): ");
   Serial.print(temperatureC2, 1);
   Serial.println(" *C");
-  buttonstate = digitalRead(11);
-  if (buttonstate == HIGH)
-  {
-    //*************angry***************************
-    if (emotion == 1)
+  
+  //buttonstate = digitalRead(11);
+  
+  /*** 
+  
+  
+  There should be a function to change the 'emotion' value state when Android calls the method in emotion class 
+  
+  
+  ***/
+  
+
+
+    switch(emotion)   //*************anger***************************
     {
-      FastLED.setTemperature( TEMPERATURE_1 ); // first temperature
-      leds[0] = TEMPERATURE_1;
-      leds[1] = TEMPERATURE_1;
-      FastLED.show();
-      for (i = 0; i < 150; i++)
+      case 1:
       {
-        digitalWrite(9, LOW); // LEFT_DOWN
-        digitalWrite(5, LOW); // RIGHT_DOWN
-        digitalWrite(10, HIGH); //LEFT_UPPER
-        digitalWrite(6, HIGH); // RIGHT_UPPER
-        delay(20);
+        leds[0] = CRGB::Red; 
+        leds[1] = CRGB::Red; 
+        FastLED.show();
+        for (i = 0; i < 150; i++)
+        {
+          digitalWrite(9, LOW); // LEFT_DOWN
+          digitalWrite(5, LOW); // RIGHT_DOWN
+          digitalWrite(10, HIGH); //LEFT_UPPER
+          digitalWrite(6, HIGH); // RIGHT_UPPER
+          delay(20);
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(0);
+        }
+        for (i = 0; i < 30; i++)
+        {
+          digitalWrite(9, HIGH);
+          digitalWrite(5, HIGH);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(20);
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(0);
+        }
+      }break;
+      
+      case 2: // *************Happy*********************
+      {
+        leds[0] = CRGB::Yellow; 
+        leds[1] = CRGB::Yellow; 
+        FastLED.show();
+        for (i = 0; i < 10; i++)
+        {
+          digitalWrite(9, HIGH);
+          digitalWrite(5, HIGH);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(20);
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(0);
+        }
+        for (i = 0; i < 10; i++)
+        {
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, HIGH);
+          digitalWrite(6, HIGH);
+          delay(20);
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(0);
+        }
+
+      }break;
+        
+      case 3:  //*****************Sad***************************
+      {
+        FastLED.setTemperature( TEMPERATURE_3 ); // first temperature
+        leds[0] = CRGB::Blue; 
+        leds[1] = CRGB::Blue;
+        FastLED.show();
+        for (i = 0; i < 200; i++)
+        {
+          digitalWrite(9, HIGH);
+          digitalWrite(5, HIGH);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(20);
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(0);
+        }
+        for (i = 0; i < 50; i++)
+        {
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, HIGH);
+          digitalWrite(6, HIGH);
+          delay(20);
+          digitalWrite(9, LOW);
+          digitalWrite(5, LOW);
+          digitalWrite(10, LOW);
+          digitalWrite(6, LOW);
+          delay(0);
+        }
+      }break;
+      
+      case 4: //*************Fear*********************
+      {
+        leds[0] = CRGB::Green; 
+        leds[1] = CRGB::Green; // add dimming + blink 
+        FastLED.show();
+        for (i = 0; i < 10; i++)
+        {
+         
+         
+        }
+        for (i = 0; i < 10; i++)
+        {
+         
+          
+        }
+      }break;
+
+      case 5:  //************disgust***********
+      {
+        leds[0] = CRGB::Purple 
+        leds[1] = CRGB::Purple; 
+        FastLED.show();
+        
+      }break;
+
+       case 6: //************suprise***********
+      {
+        leds[0] = CRGB::Orange; 
+        leds[1] = CRGB::Orange; // add blink 
+        FastLED.show();
+        
+      }break;
+
+       default: // **************** steady state ********************
+      {
         digitalWrite(9, LOW);
         digitalWrite(5, LOW);
         digitalWrite(10, LOW);
         digitalWrite(6, LOW);
-        delay(0);
       }
-      for (i = 0; i < 30; i++)
-      {
-        digitalWrite(9, HIGH);
-        digitalWrite(5, HIGH);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(20);
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(0);
-      }
+        
     }
-    else if (emotion == 2)
-    { //*************Happy*********************
-      FastLED.setTemperature( TEMPERATURE_2 ); // first temperature
-      leds[0] = TEMPERATURE_2;
-      leds[1] = TEMPERATURE_2;
-      FastLED.show();
-      for (i = 0; i < 10; i++)
-      {
-        digitalWrite(9, HIGH);
-        digitalWrite(5, HIGH);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(20);
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(0);
-      }
-      for (i = 0; i < 10; i++)
-      {
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, HIGH);
-        digitalWrite(6, HIGH);
-        delay(20);
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(0);
-      }
-    }
-    else if (emotion == 3)    //*****************Sad***************************
-    {
-      FastLED.setTemperature( TEMPERATURE_3 ); // first temperature
-      leds[0] = TEMPERATURE_3;
-      leds[1] = TEMPERATURE_3;
-      FastLED.show();
-      for (i = 0; i < 200; i++)
-      {
-        digitalWrite(9, HIGH);
-        digitalWrite(5, HIGH);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(20);
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(0);
-      }
-      for (i = 0; i < 50; i++)
-      {
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, HIGH);
-        digitalWrite(6, HIGH);
-        delay(20);
-        digitalWrite(9, LOW);
-        digitalWrite(5, LOW);
-        digitalWrite(10, LOW);
-        digitalWrite(6, LOW);
-        delay(0);
-      }
-    }
-  }
-  else
-  {
-    digitalWrite(9, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(6, LOW);
-  }
 
 }
+
 
